530fc6a57633742ab83dd6390bdb5aedc5de940e
CindyAzu <34848703+CindyAzu@users.noreply.github.com>
2018-02-12 02:36:46 +0100

    Delete header.lua

--- StarAlistar.jip/header.lua
+++
@@ -1,9 +0,0 @@
-return {
-	["scriptType"] = "Champion",
-	["scriptName"] = "StarAlistar",
-	["moduleName"] = "StarAlistar",
-	["entryPoint"] = "main.lua",
-	["loadToCoreMenu"] = function()
-		return player.charName == "Alistar"
-	end
-}
